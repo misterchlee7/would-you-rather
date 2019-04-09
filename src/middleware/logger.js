5c0015448b88de81fd767ea60081ff7f9a9c0a3b
Chang Lee <cleeucla@gmail.com>
2019-04-08 22:48:28 -0700

    init react-redux

---
+++ src/middleware/logger.js
@@ -0,0 +1,9 @@
+const logger = store => next => action => {
+  console.group(action.type);
+  console.log('The action: ', action)
+  console.log('The new state: ', store.getState())
+  console.groundEnd();
+  return next(action);
+}
+
+export default logger;
