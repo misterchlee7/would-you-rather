fb073e2f5666751f567fedbff9a0f15fe0dbc7f9
Chang Lee <cleeucla@gmail.com>
2019-04-16 15:22:38 -0700

    receive all questions when logged in to dashboard

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleReceiveQuestions } from '../actions/questions';
 
 class Dashboard extends Component {
+  componentDidMount() {
+    this.props.receiveQuestions();
+  }
+
   render() { 
+    const questionList = this.props.questionList.length === 0
+      ? <div>Loading...</div>
+      : this.props.questionList.map(q => <p>{q}</p>)
+
     return (
       <div>
         Dashboard
+        {questionList}
       </div>
     );
   }
 }
+
+const mapStateToProps = ({ questions }) => ({
+  questionList: Object.keys(questions),
+});
+
+const mapDispatchToProps = dispatch => ({
+  receiveQuestions: () => dispatch(handleReceiveQuestions())
+})
  
-export default Dashboard;
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,18 @@
-export const { RECEIVE_QUESTIONS } = 'RECEIVE_QUESTIONS';
+import { _getQuestions } from '../utils/_DATA';
 
-export function receiveQuestions(questions) {
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+
+function receiveQuestions(questions) {
   return {
     type: RECEIVE_QUESTIONS,
     questions,
   }
-};
+}
+
+export function handleReceiveQuestions() {
+  return dispatch => {
+    return _getQuestions()
+      .then(questions => dispatch(receiveQuestions(questions)))
+      // .then(questions => console.log('QUESTIONS', questions))
+  }
+}
